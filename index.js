7ce22b874db7c4c45bda7a48f1f7c26d777329ac
Rudy Marca <rudy.marca@viacontours.com>
2023-12-18 22:59:17 -0400

    Deployment a Heroku

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const cors = require('cors');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 // const whiteList = ['https://myapp.com'];
 // const optionCors = {
 //   origin: (origin, callback) => {
-//     if (whiteList.includes(origin)) {
+//     if (whiteList.includes(origin) || !origin) {
 //       callback(null, true);
 //     } else {
 //       callback(new Error('no permitido'));
